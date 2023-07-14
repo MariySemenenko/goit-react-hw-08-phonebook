15076fe0c8af2f0d4d2881910e07f30608011f4e
MariySemenenko <mashyni92@gmail.com>
2023-07-14 11:54:40 +0300

    //

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,13 @@
-//import { Helmet} from 'react-helmet-async';// для додавання метатегу title 
-import { LoginForm } from 'components/loginForm/LoginForm';
-import RegisterForm from 'components/RegisterForm/RegisterForm';
+import { Helmet} from 'react-helmet-async';// для додавання метатегу title 
+import { LoginForm } from 'components/LoginForm/LoginForm';
+//import RegisterForm from 'components/RegisterForm/RegisterForm';
 
 export default function Login() {
   return (
     <div>
-      <RegisterForm>
+      <Helmet>
         <title>Login</title>
-      </RegisterForm>
+      </Helmet>
       <LoginForm />
     </div>
   );
